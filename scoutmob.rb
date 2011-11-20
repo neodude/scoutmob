f577ad5c5efd215d92f96b88aeda7d64cd04b9a9
Thomas Bukowski <me@neodude.net>
2011-11-19 21:12:39 -0800

    get map working, use ajax to fetch data

--- scoutmob.rb
+++ scoutmob.rb
@@ -3,10 +3,32 @@ require 'rubygems'
 require 'sinatra'
 require "sinatra/reloader" if development?
 require 'nokogiri'
+require 'haml'
+require 'json'
+
+helpers do
+  def extract_children_from_nodeset(nodeset, attrs)
+    nodeset.map do |node|
+      children = node.children.filter(attrs.join(','))
+      hash = {}
+      children.each do |child|
+        hash[child.name.to_sym] = child.content
+      end
+      hash
+    end
+  end
+end
 
 get '/' do
+  haml :index, :format => :html5
+end
+
+get '/data.json' do
   rss = File.open('./sample.rss')
   feed = Nokogiri::XML.parse(rss.read)
   
-  feed.search('item:has(percentageOff) title').map(&:content).join("<br>\n")
+  items = feed.search('item:has(percentageOff)')
+  
+  @data = extract_children_from_nodeset(items, ['latitude', 'longitude', 'title'])
+  @data.to_json
 end
